fd9eaf7e6ed8101034c16cf07a86bd57f0372d3f
Williams25 <william.souza.timao@hotmail.com>
2024-09-03 20:06:05 -0300

    config project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Salvar</Button>
+    </div>
+  );
+};
