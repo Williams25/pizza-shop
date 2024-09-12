284510521776c990f687800bfeedd7c6e9bd7f87
Williams25 <william.souza.timao@hotmail.com>
2024-09-12 20:06:17 -0300

    feat: mudando status do pedido

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -2,14 +2,17 @@ import { Button } from '@/components/ui/button';
 import { TableCell, TableRow } from '@/components/ui/table';
 import { ArrowRight, Search, X } from 'lucide-react';
 import { OrderDetails } from './order-details';
-import { GetOrdersResponse, Order } from '@/api/get-orders';
+import { GetOrdersResponse, Order, StatusOrder } from '@/api/get-orders';
 import { OrderStatus } from './order-status';
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { cancelOrder } from '@/api/cancel-order';
+import { approveOrder } from '@/api/approve-order';
+import { deliverOrder } from '@/api/deliver-order';
+import { dispatchOrder } from '@/api/dispatch-order';
 
 export interface IOrderTableRowProps {
   order: Order;
@@ -19,25 +22,82 @@ export const OrderTableRow = ({ order }: IOrderTableRowProps) => {
   const [isDetailsOpen, setIsDetailsOpen] = useState<boolean>(false);
   const queryClient = useQueryClient();
 
-  const { mutateAsync: cancelOrderFn } = useMutation({
-    mutationFn: cancelOrder,
-    onSuccess: (_, { orderId }) => {
-      const cached = queryClient.getQueriesData<GetOrdersResponse>({
-        queryKey: ['getOrders'],
-      });
-      cached.forEach(([cacheKey, cacheData]) => {
-        if (!cacheData) return;
+  const updateStatusOrderOnCache = (orderId: string, status: StatusOrder) => {
+    const cached = queryClient.getQueriesData<GetOrdersResponse>({
+      queryKey: ['getOrders'],
+    });
+    cached.forEach(([cacheKey, cacheData]) => {
+      if (!cacheData) return;
 
-        queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
-          ...cacheData,
-          orders: cacheData.orders.map((order) => ({
-            ...order,
-            status: order.orderId === orderId ? 'canceled' : order.status,
-          })),
-        });
+      queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
+        ...cacheData,
+        orders: cacheData.orders.map((order) => ({
+          ...order,
+          status: order.orderId === orderId ? status : order.status,
+        })),
       });
-    },
-  });
+    });
+  };
+
+  const { mutateAsync: cancelOrderFn, isPending: isCancelingOrder } =
+    useMutation({
+      mutationFn: cancelOrder,
+      onSuccess: (_, { orderId }) => {
+        updateStatusOrderOnCache(orderId, 'canceled');
+      },
+    });
+
+  const { mutateAsync: approveOrderFn, isPending: isApprovingOrder } =
+    useMutation({
+      mutationFn: approveOrder,
+      onSuccess: (_, { orderId }) => {
+        updateStatusOrderOnCache(orderId, 'processing');
+      },
+    });
+
+  const { mutateAsync: dispatchOrderFn, isPending: isDispatchingOrder } =
+    useMutation({
+      mutationFn: dispatchOrder,
+      onSuccess: (_, { orderId }) => {
+        updateStatusOrderOnCache(orderId, 'delivering');
+      },
+    });
+
+  const { mutateAsync: deliverOrderFn, isPending: isDeliveringOrder } =
+    useMutation({
+      mutationFn: deliverOrder,
+      onSuccess: (_, { orderId }) => {
+        updateStatusOrderOnCache(orderId, 'delivered');
+      },
+    });
+
+  const labelApproveButton = useMemo(() => {
+    if (order.status === 'pending') {
+      return {
+        label: 'Aprovar',
+        isPending: isApprovingOrder,
+        onMutate: approveOrderFn,
+      };
+    }
+
+    if (order.status === 'processing') {
+      return {
+        label: 'Em entrega',
+        isPending: isDispatchingOrder,
+        onMutate: dispatchOrderFn,
+      };
+    }
+
+    if (order.status === 'delivering') {
+      return {
+        label: 'Entregue',
+        isPending: isDeliveringOrder,
+        onMutate: deliverOrderFn,
+      };
+    }
+
+    return null;
+  }, [order.status, isApprovingOrder, isDeliveringOrder, isDispatchingOrder]);
 
   return (
     <TableRow>
@@ -79,17 +139,29 @@ export const OrderTableRow = ({ order }: IOrderTableRowProps) => {
       </TableCell>
 
       <TableCell>
-        <Button variant={'outline'} size={'xs'}>
-          <ArrowRight className="mr-2 h-3 w-3" />
-          Aprovar
-        </Button>
+        {labelApproveButton && (
+          <Button
+            variant={'outline'}
+            size={'xs'}
+            disabled={labelApproveButton.isPending}
+            onClick={() =>
+              labelApproveButton.onMutate({ orderId: order.orderId })
+            }
+          >
+            <ArrowRight className="mr-2 h-3 w-3" />
+            {labelApproveButton.label}
+          </Button>
+        )}
       </TableCell>
 
       <TableCell>
         <Button
           variant={'ghost'}
           size={'xs'}
-          disabled={!['processing', 'pending'].includes(order.status)}
+          disabled={
+            !['processing', 'pending'].includes(order.status) ||
+            isCancelingOrder
+          }
           onClick={() => cancelOrderFn({ orderId: order.orderId })}
         >
           <X className="mr-2 h-3 w-3" />
