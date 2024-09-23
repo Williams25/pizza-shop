f83b24272655d3f02f6864bd6bcb5de299a8a71e
Williams25 <william.souza.timao@hotmail.com>
2024-09-23 14:14:02 -0300

    exportando enums

--- src/pages/app/orders/order-status.tsx
+++ src/pages/app/orders/order-status.tsx
@@ -4,7 +4,7 @@ export interface IOrderStatusProps {
   status: StatusOrder;
 }
 
-const orderStatusMap: Record<StatusOrder, string> = {
+export const orderStatusMap: Record<StatusOrder, string> = {
   canceled: 'Cancelado',
   delivered: 'Entregue',
   delivering: 'Em entrega',
@@ -12,7 +12,7 @@ const orderStatusMap: Record<StatusOrder, string> = {
   processing: 'Em preparo',
 };
 
-const orderStatusColorMap: Record<StatusOrder, string> = {
+export const orderStatusColorMap: Record<StatusOrder, string> = {
   canceled: 'bg-rose-500',
   delivered: 'bg-emerald-500',
   delivering: 'bg-amber-400',
@@ -23,7 +23,10 @@ const orderStatusColorMap: Record<StatusOrder, string> = {
 export const OrderStatus = ({ status }: IOrderStatusProps) => {
   return (
     <div className="flex items-center gap-2">
-      <span className={`h-2 w-2 rounded-full ${orderStatusColorMap[status]}`} />
+      <span
+        className={`h-2 w-2 rounded-full ${orderStatusColorMap[status]}`}
+        data-testid="badge"
+      />
       <span className="font-medium text-muted-foreground">
         {orderStatusMap[status]}
       </span>
